009e90b2d795e8a4412e4590c482d1ce8b50d30c
Ben Bullock <benkasminbullock@gmail.com>
2021-02-21 09:08:43 +0900

    Version to 0.02

--- versionup.pl
+++ versionup.pl
@@ -10,8 +10,8 @@ use FindBin '$Bin';
 use File::Slurper qw!read_text write_text!;
 use Deploy 'make_date';
 
-my $version = '0.01';
-my $newversion = '0.01_01';
+my $newversion = '0.02';
+my $version = '0.01_01';
 
 my @pmfiles = qw!
     lib/Gzip/Libdeflate.pm
